bec52067282c854edf291fbfb750ed72cb453c2a
Junaid Rasheed <jrasheed@proda.ai>
2025-05-28 23:58:24 +0100

    lsp booster and fix lsp keymap prefix

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,10 @@
 ;; -*- lexical-binding: t -*-
 (require 'package)
 
+;; Config
+(setenv "LSP_USE_PLISTS" "true")
+
+;; Packages
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
@@ -541,8 +545,42 @@
                          (lsp-deferred))))
 
 (use-package lsp-mode
+  :init
+  ;; lsp booster setup
+  (defun lsp-booster--advice-json-parse (old-fn &rest args)
+    "Try to parse bytecode instead of json."
+    (or
+     (when (equal (following-char) ?#)
+       (let ((bytecode (read (current-buffer))))
+         (when (byte-code-function-p bytecode)
+           (funcall bytecode))))
+     (apply old-fn args)))
+  (advice-add (if (progn (require 'json)
+                         (fboundp 'json-parse-buffer))
+                  'json-parse-buffer
+                'json-read)
+              :around
+              #'lsp-booster--advice-json-parse)
+
+  (defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+    "Prepend emacs-lsp-booster command to lsp CMD."
+    (let ((orig-result (funcall old-fn cmd test?)))
+      (if (and (not test?)                             ;; for check lsp-server-present?
+               (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+               lsp-use-plists
+               (not (functionp 'json-rpc-connection))  ;; native json-rpc
+               (executable-find "emacs-lsp-booster"))
+          (progn
+            (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+              (setcar orig-result command-from-exec-path))
+            (message "Using emacs-lsp-booster for %s!" orig-result)
+            (cons "emacs-lsp-booster" orig-result))
+        orig-result)))
+  (advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
+
+  (setq lsp-keymap-prefix "C-c c")
   :config
-  (setopt lsp-keymap-prefix "C-c c")
+  (define-key lsp-mode-map (kbd "C-c c") lsp-command-map)
   :hook (
 	 (haskell-mode . lsp-deferred)
 	 (haskell-literature-mode . lsp-deferred)
